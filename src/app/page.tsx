fead58207268e30d99e58e0ac71523417fc8f724
Salwa Jeries <annette.jeries@gmail.com>
2024-05-28 15:21:28 -0500

    Added the tie fighter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ import "./globals.css";
 import About from "./about/about";
 import Projects from "./projects/projects";
 import useMediaQuery from "./hooks/use-media-query";
+import TieFighter from "@/components/TieFighter";
 
 export default function Home() {
   const isDesktop = useMediaQuery("(min-width: 786px)");
 
   const [scrollPosition, setScrollPosition] = useState(0);
-  const initialXPositionVW = 25; // Initial horizontal position in viewport width units (vw)
+  const initialXPositionVW = 30; // Initial horizontal position in viewport width units (vw)
   const maxScroll = 1000; // Maximum scroll value at which position stops changing
   const maxOffsetVW = 150; // The horizontal offset (in vw) needed to move the element completely off the screen
 
@@ -35,7 +36,7 @@ export default function Home() {
   if (isDesktop) {
     // FOR DESKTOP
     // Calculate the horizontal position of the div in vw units
-    const maxScroll = 950; // Maximum scroll value at which position stops changing
+    const maxScroll = 1100; // Maximum scroll value at which position stops changing
     horizontalPositionVW = Math.max(
       0,
       initialXPositionVW - (scrollPosition / maxScroll) * initialXPositionVW
@@ -45,7 +46,7 @@ export default function Home() {
     // Calculate the horizontal position of the div in vw units
     horizontalPositionVW = Math.max(
       -maxOffsetVW,
-      initialXPositionVW - 3 - (scrollPosition / maxScroll) * maxOffsetVW
+      initialXPositionVW - (scrollPosition / maxScroll) * maxOffsetVW
     );
   }
 
@@ -53,15 +54,16 @@ export default function Home() {
     <div>
       <div
         style={{ transform: `translateX(calc(${horizontalPositionVW}vw))` }}
-        className="fixed flex h-[100vh] items-center justify-center ml-10"
+        className="fixed flex h-[100vh] items-center justify-center"
       >
-        <div className="flex-col justify-center w-[50vw]">
-          <TypewriterTitle title="hi, i'm salwa" size="7vw" />
+        <div className="flex flex-col items-center justify-center w-[40vw]">
+          <TieFighter />
+          <TypewriterTitle title="hi, i'm salwa" size="6vw" />
           <div
             style={{
               fontSize: `${isDesktop ? "2vw" : "1em"}`,
             }}
-            className="w-full"
+            className=""
           >
             <Typewriter
               options={{
---
+++ src/components/TieFighter.tsx
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+
+const TieFighterModel: React.FC = () => {
+  const { scene } = useGLTF("/TieFighter.glb");
+  const modelRef = useRef<THREE.Object3D>();
+
+  // Use useFrame to rotate the model
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.005; // Adjust the speed of rotation here
+    }
+  });
+
+  return <primitive ref={modelRef} object={scene} scale={[0.5, 0.5, 0.5]} />;
+};
+
+const TieFighter: React.FC = () => {
+  const cameraRef = useRef<THREE.PerspectiveCamera>(null!);
+
+  useEffect(() => {
+    if (cameraRef.current) {
+      cameraRef.current.lookAt(0, 0, 0);
+    }
+  }, []);
+
+  return (
+    <Canvas
+      camera={{ position: [3, 2, 5], near: 0.1, far: 1000 }}
+      onCreated={({ camera }) => {
+        cameraRef.current = camera as THREE.PerspectiveCamera;
+      }}
+      className="h-full w-full"
+    >
+      {/* Increase the intensity of the ambient light */}
+      <ambientLight intensity={1} />
+
+      {/* Increase the intensity of the point light */}
+      <pointLight position={[10, 10, 10]} intensity={2} />
+
+      <TieFighterModel />
+      <OrbitControls enableZoom={false} enablePan={false} />
+    </Canvas>
+  );
+};
+
+export default TieFighter;
