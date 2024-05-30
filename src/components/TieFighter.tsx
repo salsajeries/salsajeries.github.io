198b4e1b360629f18587d0cd0f5579973502970c
Salwa Jeries <annette.jeries@gmail.com>
2024-05-29 21:26:10 -0500

    I will deal with this later.

--- src/components/TieFighter.tsx
+++ src/components/TieFighter.tsx
@@ -1,11 +1,14 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useSpring, animated } from "@react-spring/three";
 import * as THREE from "three";
 
 const TieFighterModel: React.FC = () => {
   const { scene } = useGLTF("/TieFighter.glb");
   const modelRef = useRef<THREE.Object3D>();
+  const [hovered, setHovered] = useState(false);
+  const [pressed, setPressed] = useState(false);
 
   // Use useFrame to rotate the model
   useFrame(() => {
@@ -14,7 +17,34 @@ const TieFighterModel: React.FC = () => {
     }
   });
 
-  return <primitive ref={modelRef} object={scene} scale={[0.5, 0.5, 0.5]} />;
+  // Spring for smooth scale transition
+  const { scale } = useSpring({
+    scale: pressed
+      ? [0.4, 0.4, 0.4]
+      : hovered
+      ? [0.6, 0.6, 0.6]
+      : [0.5, 0.5, 0.5], // Scale up when hovered
+    config: { tension: 300, friction: 30 }, // Adjust config for smoothness
+  });
+
+  return (
+    <group
+      onPointerEnter={() => setHovered(true)} // Set hovered to true on pointer over
+      onPointerLeave={() => {
+        setHovered(false);
+        setPressed(false); // Ensure pressed is reset on pointer out
+      }} // Set hovered to false on pointer out
+      onPointerDown={() => setPressed(true)} // Set pressed to true on pointer down
+      onPointerUp={() => setPressed(false)} // Set pressed to false on pointer up
+    >
+      <animated.group
+        ref={modelRef}
+        scale={scale} // Use spring scale
+      >
+        <primitive object={scene} />
+      </animated.group>
+    </group>
+  );
 };
 
 const TieFighter: React.FC = () => {
@@ -38,7 +68,6 @@ const TieFighter: React.FC = () => {
 
       {/* Increase the intensity of the point light */}
       <pointLight position={[10, 10, 10]} intensity={2} />
-
       <TieFighterModel />
       <OrbitControls enablePan={false} enableZoom={false} />
     </Canvas>
