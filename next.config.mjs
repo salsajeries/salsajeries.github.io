106d1a0096606e7d9a038df49654024d2d80447d
Salwa Jeries <annette.jeries@gmail.com>
2024-06-01 02:54:20 -0500

    Fixed export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/personal-site",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
