8a4ca53965ccac0ad319a968fab50b0ba2e8ac94
Salwa Jeries <annette.jeries@gmail.com>
2025-03-25 14:01:41 -0500

    Added footer and blog section

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col mt-10 mb-10 justify-center items-center">
+      <p>Copyright © 2024 Salwa Jeries</p>
+    </div>
+  );
+}
