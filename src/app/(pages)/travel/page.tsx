cfa84944990b2154bd94c2125d2f46130e229bbf
Salwa Jeries <annette.jeries@gmail.com>
2025-03-26 18:35:49 -0500

    Took out of travel page, renamed to card stack

--- src/app/(pages)/travel/page.tsx
+++ src/app/(pages)/travel/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import CardDeckCarousel from "@/_components/carddeck/CardDeckCarousel";
-
 export default function Travel() {
   return (
     <div>
-      <CardDeckCarousel />
     </div>
   );
 }
